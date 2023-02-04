cfb7ab68cb61da3f9d8f2426a3273ab61c9973f8
sivadotblog <hello@siva.blog>
2023-02-04 12:03:24 -0500

    updated

--- .devcontainer/initialize.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-export HOME=/
-
-echo " # This is the user config file" > $HOME/.gitconfig
-git config --global --add safe.directory /var/lib/docker/codespacemount/workspace/data-platform-handbook/themes/hugo-theme-relearn
-git submodule update --init
